800801f3360533b3f94cb69444a97fc963ddb286
nevaselma <hurigulsum@gmail.com>
2018-06-09 16:20:14 -0500

    etsy search automation

---
+++ src/com/cbt/GoogleSearch.java
@@ -0,0 +1,18 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\hurig\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://etsy.com");
+			
+	}
+
+}
